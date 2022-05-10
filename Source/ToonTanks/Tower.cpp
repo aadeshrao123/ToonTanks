273dde4827dfa4a414c826af13e9c5fb6b8a2e3d
aadeshrao123 <55663044+aadeshrao123@users.noreply.github.com>
2022-05-10 14:18:34 +0530

    Class 130  I moved the barrel of tower towards the tank
    Class 130
    In this I made a tower class by the base pawn like done for the Tank.
    I made the tower to move its barrel towards me when I am in range.

---
+++ Source/ToonTanks/Tower.cpp
@@ -0,0 +1,28 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+
+#include "Tower.h"
+#include "Tank.h"
+#include "Kismet/GameplayStatics.h"
+
+
+
+void ATower::BeginPlay()
+{
+	Super::BeginPlay();
+	Tank = Cast<ATank>(UGameplayStatics::GetPlayerPawn(GetWorld(), 0));
+}
+
+
+void ATower::Tick(float DeltaTime)
+{
+	Super::Tick(DeltaTime);
+	if (Tank)
+	{
+		float Distance = FVector::Dist(GetActorLocation(), Tank->GetActorLocation());
+		if (Distance <= FireRange)
+		{
+			TurretRotation(Tank->GetActorLocation());
+		}
+	}
+}
