4ed17099973f32687e2b2e88f9c1c7a3cfb7b973
Aadesh Yadav <55663044+aadeshrao123@users.noreply.github.com>
2022-05-14 16:08:27 +0530

    Class 136 Hit Events
    I don't know what the hell is this and how it works maybe future self will be able to figure it out
    
    go watch the class again to understand it

--- Source/ToonTanks/Projectile.cpp
+++ Source/ToonTanks/Projectile.cpp
@@ -25,6 +25,7 @@ AProjectile::AProjectile()
 void AProjectile::BeginPlay()
 {
 	Super::BeginPlay();
+	ProjectileMesh->OnComponentHit.AddDynamic(this, &AProjectile::OnHit);
 	
 }
 
@@ -33,5 +34,10 @@ void AProjectile::Tick(float DeltaTime)
 {
 	Super::Tick(DeltaTime);
 
+}
+
+void AProjectile::OnHit(UPrimitiveComponent* HitComp, AActor* OtherActor, UPrimitiveComponent* OtherComp, FVector NormalImpulse, const FHitResult& Hit)
+{
+	UE_LOG(LogTemp, Warning, TEXT("HitComp: %s, OtherActor: %s, OtherComp: %s"), *HitComp->GetName(), *OtherActor->GetName(), *OtherComp->GetName());
 }
 
--- Source/ToonTanks/Projectile.h
+++ Source/ToonTanks/Projectile.h
@@ -30,4 +30,7 @@ private:
 	UPROPERTY(VisibleAnywhere, Category = "Movement")
 		class UProjectileMovementComponent* ProjectileMovement;
 
+	UFUNCTION()
+		void OnHit(UPrimitiveComponent* HitComp, AActor* OtherActor, UPrimitiveComponent* OtherComp, FVector NormalImpulse, const FHitResult& Hit);
+
 };
