882c941754a6b67dc4c698928ccac2b6d1254bdd
Aadesh Yadav <55663044+aadeshrao123@users.noreply.github.com>
2022-05-18 21:59:19 +0530

    Class Hit Particles
    Class Hit Particles
    
    In this class I have added a smoke after after projectile hit something I use the UGamePlayStatics::SpawnEmitterAtLocatin function for this.

--- Source/ToonTanks/Projectile.cpp
+++ Source/ToonTanks/Projectile.cpp
@@ -42,7 +42,11 @@ void AProjectile::Tick(float DeltaTime)
 void AProjectile::OnHit(UPrimitiveComponent* HitComp, AActor* OtherActor, UPrimitiveComponent* OtherComp, FVector NormalImpulse, const FHitResult& Hit)
 {
 	auto MyOwner = GetOwner();
-	if (MyOwner == nullptr) return;
+	if (MyOwner == nullptr)
+	{
+		Destroy();
+		return;
+	}
 
 	auto MyOwnerInstigator = MyOwner->GetInstigatorController();
 
@@ -51,7 +55,11 @@ void AProjectile::OnHit(UPrimitiveComponent* HitComp, AActor* OtherActor, UPrimi
 	if (OtherActor && OtherActor != this && OtherActor != MyOwner)
 	{
 		UGameplayStatics::ApplyDamage(OtherActor, Damage, MyOwnerInstigator, this, DamageType);
-		Destroy();
+		if (HitParticles)
+		{
+		UGameplayStatics::SpawnEmitterAtLocation(this, HitParticles, GetActorLocation(), GetActorRotation());
+		}
 	}
+	Destroy();
 }
 
--- Source/ToonTanks/Projectile.h
+++ Source/ToonTanks/Projectile.h
@@ -35,4 +35,7 @@ private:
 
 	UPROPERTY(EditAnywhere)
 		float Damage = 50.f;
+	
+	UPROPERTY(EditAnywhere, Category = "Combat")
+		UParticleSystem* HitParticles;
 };
